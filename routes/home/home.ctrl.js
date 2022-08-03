a90471755233a313a9d4afa602609dc1caf1f1b2
Youngsuk <djfls0304@naver.com>
2022-08-03 20:14:56 +0900

    controller 분리
    index.js 에있는 컨트롤러 부분을
    home.ctrl.js 파일로 분리

---
+++ routes/home/home.ctrl.js
@@ -0,0 +1,25 @@
+"use strict"
+
+const hello=(req, res) =>{
+  res.render("home/index");
+}
+
+const login=(req, res) =>{
+  res.render("home/login");
+}
+
+//모듈을 밖으로 빼주기 함수의 return 같은 느낌
+module.exports ={
+  hello,
+  login,
+}
+
+//설명 
+//오브젝트에서 일반적으로 
+//{key:value}형태를 사용한다.
+//하지만 위 처럼 key만 입력 해줄 경우
+//아래와 같이 key와 value가 같은 값으로 설정
+// {
+//   hello : hello,
+//   lonin : login,
+// } 
--- routes/home/index.js
+++ routes/home/index.js
@@ -4,18 +4,17 @@ const express = require("express");
 //app이 아닌 router을 사용한다
 const router = express.Router();
 
-//그래서 app.get()을 사용하여 경로를 정해줘야한다.
-// app.get("/",(req,res)=>{
-//   res.render("home/index")
-// });
-//하지만 파일을 따로 빼주면서 app이 아닌 router 로 사용 
-router.get("/",(req,res)=>{
-  res.render("home/index")
-});
+//ctrl에서 빼준 부분 을 사용가능하게 받기.
+const ctrl = require("./home.ctrl");
 
-router.get("/login",(req,res)=>{
-  res.render("home/login")
-});
+// router.get("/",(req,res)=>{
+//   res.render("home/index");
+// }); 
+//위 코드에서 (req,res)=>{res.render("home/index"); 부분이 컨트롤러에 해당
+//컨트롤러 부분을 제거하고 빼줄 예정
+router.get("/",ctrl.hello);
+
+router.get("/login",ctrl.login);
 
 //router를 외부파일에서 사용할 수 있도록 던져주는 부분
 module.exports = router;
