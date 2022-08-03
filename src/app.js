472c9a0b124426782f87ee7ef66e4a814b089583
Youngsuk <djfls0304@naver.com>
2022-08-03 22:32:24 +0900

    소스코드 src 폴더로 분류

--- src/app.js
+++ src/app.js
@@ -1,19 +1,4 @@
 "use strict"; //엄격모드를 의미하며 이코마 스크립트 문법을 준수하겠다고 명시 해주는것. js파일 위에 넣는다.
-//설명 
-
-//express 모듈을 설치하기 위해서
-//npm install express --save 를 터미널에 입력
-//위의 명령어를 수행해주면 node_modules라는 dir과 package-lock.json, package.json 파일이 다운로드 되며
-//node_modules에서 express라는 모델과 express모델을 사용하기 위해 필요한 다른것들까지 설치되는것을 확인 가능
-
-//view 엔진 세팅(앱 세팅)
-//express처럼 ejs 엔진을 사용하기 위해선 
-//npm install ejs --save를 해서 설치 해줘야한다.
-
-//서버 실행 - 프로그램을 실행(node app.js)하면 listen(포트번호)해당 포트로 서버가 열린다.
-//서버 종료법 - 가끔 종료가 제대로 되지 않아서 port가 계속 사용되는 경우가 있다.
-//이럴때는 cmd 창에 netstat -ano | findstr LISTEN 를 입력후 해당 port가 사용되고 있는 pid를 찾아
-//taskkill /f /pid pid번호 를 입력해주면 해당 포트사용을 중지 할 수 있다.
 
 //모듈
 //express 모듈을 이용한 서버 열기
