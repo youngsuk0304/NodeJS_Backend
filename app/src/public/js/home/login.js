7c307ac74cca7d5d6ed14fd986c2c548f43cb0b5
Youngsuk <djfls0304@naver.com>
2022-08-04 22:38:51 +0900

    DOM을 이용하여 HTML 객체 저장
    document.querySelector(””);는 질의 선택자로 안에 지정해주는 값을 의미한다.
    document.querySelector(”#id”); #id는 id의 값이 id인 질의를 가져오겠다는 의미이다.
    만약
    document.querySelector(”id”);일 경우 태그가 id인 태그인 값을 가져온다.
    
    여기서 문제점은 login.js 파일이 document.querySelector()파일을 시작하기 전에 console.log(id);를 먼저 실행해서 값을 못가져 오게 된다는것.
    
    이 문제를 해결 하기 위해서는 lonin.ejs 파일에서
    <script src="/js/home/login.js" defer></script>
    js파일을 불러올떄 defer라는 옵션을 부여해주는것이다.
    script 에 줄 수 있는 옵션은 defer와 async 두가지가 존재한다.
    
    defer
    - 브라우저는 `defer`
     속성이 있는 스크립트(이하 defer 스크립트 또는 지연 스크립트)를 '백그라운드’에서 다운로드 합니다. 따라서 지연 스크립트를 다운로드 하는 도중에도 HTML 파싱이 멈추지 않습니다. 그리고 `defer`
     스크립트 실행은 페이지 구성이 끝날 때까지 *지연*  됩니다.
    
    async
    - `async` 속성이 붙은 스크립트(이하 async 스크립트 또는 비동기 스크립트)는 페이지와 완전히 독립적으로 동작합니다.
        - async 스크립트는 defer 스크립트와 마찬가지로 백그라운드에서 다운로드됩니다. 따라서 HTML 페이지는 async 스크립트 다운이 완료되길 기다리지 않고 페이지 내 콘텐츠를 처리, 출력합니다(하지만 async 스크립트 실행중에는 HTML 파싱이 멈춥니다 – 옮긴이).
        - `DOMContentLoaded` 이벤트와 async 스크립트는 서로를 기다리지 않습니다.
            - 페이지 구성이 끝난 후에 async 스크립트 다운로딩이 끝난 경우, `DOMContentLoaded`는 async 스크립트 실행 전에 발생할 수 있습니다,
            - async 스크립트가 짧아서 페이지 구성이 끝나기 전에 다운로드 되거나 스크립트가 캐싱처리 된 경우, `DOMContentLoaded`는 `async` 스크립트 실행 후에 발생할 수도 있습니다.
        - 다른 스크립트들은 `async` 스크립트를 기다리지 않습니다. `async` 스크립트 역시 다른 스크립트들을 기다리지 않습니다.
    
        이런 특징 때문에 페이지에 `async` 스크립트가 여러 개 있는 경우, 그 실행 순서가 제각각이 됩니다. 실행은 다운로드가 끝난 스크립트 순으로 진행됩니다.

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,19 @@
 "use strict"
 
-console.log("hello");
-console.log("bye");
+//DOM -> Document Object Model (문서 객체 모델 ) 
+//일종의 인터페이스
+
+const id = document.querySelector("#id");//질의 선택자
+const psword = document.querySelector("#psword");//#은 태그의 id의 값 'id'를 의미
+//id = 'id'
+const loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click",login);
+
+function login(){
+  const req={
+    id : id.value,
+    psword : psword.value,
+  };
+  console.log(req);
+}
