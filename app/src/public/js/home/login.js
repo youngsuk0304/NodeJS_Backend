1d6f6be001a5fa06328525e1a08d09e6483d719a
Youngsuk <djfls0304@naver.com>
2022-08-05 11:30:30 +0900

    백엔드 로그인 기능 구현
    home.ctro.js
    const process 객체에 req.body.id , req.body.psword 값을 가져와 내가 만든
    user 배열에 해당 값이 있는지 확인 있으면 success : true를 return
    아닐경우 success:false를 return
    
    login.js
    fetch함수를 이용해 home.ctrl.js 에서 return 한 값을 받아 post라는 메서드로 데이터를 전달.

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -26,13 +26,8 @@ function login(){
       "Content-Type": "application/json",  //데이터 타입 명시 "application/json"
     },
     body : JSON.stringify(req),
-  });//위와 같이 설정하면 /login이라는 경로에 POST라는 메소드로 데이터르 받을 수 있는 api가 있어야 한다.
-  //두번째 파라미터로 전달할 데이터를 보내줄 수 있다.
-  //이때 데이터의 형식은 object 여야 한다
-  //JSON이라는 형식으로 데이터를 전송 할예정이기 때문에 req를 JSON 형식으로 감싸주어야 한다.
-  // JSON.stringify(req)
-  //이때 stringify는 object를 문자열로 바꿔주는 메소드
-  //body를 통해 데이터를 전달할 때는 http메서드 중에서
-  //POST라는것이 존재하며 POST라는 메서드를 통해  데이터를 전달 해주어야 한다.
-  //이러한것이 REST API에 관련된 것들
+  })
+  .then((res)=> res.json())
+  .then((res)=>console.log(res));
+  //PROMISE가 뭔지 공부해볼것
 }
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,5 +1,7 @@
 "use strict"
 
+
+
 const output={
   home:(req, res) =>{
     res.render("home/index");
@@ -9,10 +11,31 @@ const output={
     res.render("home/login");
   },
 };
+
+const users={
+  id:["서영석","강성규","허정","방선주"],
+  psword:["1234","1111","4444","2222"],
+};
+
 // login.js의 fetch()에서 POST하는 데이터를 받아 오는 부분
-const process={
+const process={//web화면에서 id와 pw에 입력한 데이터 받아옴
   login:(req,res)=>{
-    console.log(req.body);//login.js fetch()에서 body로 데이터를 전달 해주기 때문에 req.body로 받아와야한다.
+    //console.log(req.body);//login.js fetch()에서 body로 데이터를 전달 해주기 때문에 req.body로 받아와야한다.
+    const id = req.body.id,
+    psword=req.body.psword;
+    //console.log(id,psword);
+    if (users.id.includes(id)){
+      const idx= users.id.indexOf(id);
+      if(users.psword[idx]===psword){
+        return res.json({
+          success : true,
+        });
+      }
+    }
+    return res.json({
+      success:false,
+      msg: "로그인에 실패하셨습니다.",
+    });
   },
 };
 
