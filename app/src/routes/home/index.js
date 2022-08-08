772b8f7a77bc2c694765998966ade4f2872acfca
Youngsuk <djfls0304@naver.com>
2022-08-09 00:11:56 +0900

    회원가입 페이지 구현

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -1,5 +1,7 @@
 "use strict"; //엄격모드를 의미하며 이코마 스크립트 문법을 준수하겠다고 명시 해주는것. js파일 위에 넣는다.
 
+//웹페이지의 이동부분을 담당
+
 const express = require("express");
 //app이 아닌 router을 사용한다
 const router = express.Router();
@@ -9,6 +11,7 @@ const ctrl = require("./home.ctrl");
 
 router.get("/",ctrl.output.home);
 router.get("/login",ctrl.output.login);
+router.get("/register",ctrl.output.register);//회원가입
 router.post("/login",ctrl.process.login);
 
 //router를 외부파일에서 사용할 수 있도록 던져주는 부분
