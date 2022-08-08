28988bf11eeb883d968fc01be595fdfb2a338bb0
Youngsuk <djfls0304@naver.com>
2022-08-09 00:25:08 +0900

    회원가입 요청 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -4,23 +4,27 @@
 //일종의 인터페이스
 
 const id = document.querySelector("#id");//질의 선택자
+const name = document.querySelector("#name");
 const psword = document.querySelector("#psword");//#은 태그의 id의 값 'id'를 의미
+const confirmPsword = document.querySelector("#confirm-psword");
 //id = 'id'
-const loginBtn = document.querySelector("#button");
+const registerBtn = document.querySelector("a");
+console.log("test register");
+registerBtn.addEventListener("click",register);
 
-loginBtn.addEventListener("click",login);
-
-function login(){
+function register(){
   const req={
     id : id.value,
+    name : name.value,
     psword : psword.value,
+    confirmPsword:confirmPsword,
   };
   
   //console.log(req);
   //console.log(JSON.stringify(req));
 
 
-  fetch("/login",{  //내가 전달 하려는 데이터의 형식을 알려줘야한다.
+  fetch("/register",{  //내가 전달 하려는 데이터의 형식을 알려줘야한다.
     method : "POST",
     headers:{
       "Content-Type": "application/json",  //데이터 타입 명시 "application/json"
@@ -30,13 +34,13 @@ function login(){
   .then((res)=> res.json())
   .then((res)=>{
     if (res.success){
-      location.href ="/";
+      location.href ="/login";
     }else{
 
       alert(res.msg);
     }
   }).catch((err)=>{
-    console.error("로그인 중 에러 발생");
+    console.error("회원가입 중 에러 발생");
   });
   //PROMISE가 뭔지 공부해볼것
 }
